be9aed7f9652d2a8f72af3a77eebdb67dd814f7e
charifsuccess <charifsuccess@gmail.com>
2025-10-11 20:15:11 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true }, // required for next/image on static hosting
-  // optional but helps on some hosts:
-  // trailingSlash: true,
+  images: { unoptimized: true },
 };
 module.exports = nextConfig;
